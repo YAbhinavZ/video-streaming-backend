b817be561baacab6bb816feac04716823f8303f2
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-01-19 21:47:24 +0530

    fix asyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,8 @@
-const asyncHandler = (handler)=>{
- return async (req ,res, next)=>{
-    try {
-        await handler(req,res,next);
-    } catch (error) {
-       res.status(500).json({
-        message: error.message
-       })
-    } 
- }
-   
-}
-
-export {asyncHandler};
+const asyncHandler = (handler) => {
+   return (req, res, next) => {
+     Promise.resolve(handler(req, res, next)).catch(next);
+   };
+ };
+ 
+ export { asyncHandler };
+ 
