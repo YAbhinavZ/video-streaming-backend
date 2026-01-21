a01f4b1c1831a212c59438a9ddfc194fd21ca224
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-01-21 23:02:40 +0530

    fixes some bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -91,7 +91,7 @@ const loginUser = asyncHandler(async (req, res) => {
   //generate refresh and access token
   //send cookies
   const { email, username, password } = req.body;
-  if (!username || !email) {
+  if (!username && !email) {
     throw new ApiError(400, "username or email is required");
   }
   const user = await User.findOne({
